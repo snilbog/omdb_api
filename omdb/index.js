309a4538a079a9abc78b65618244958013cbab21
snilbog <fitch.neal@gmail.com>
2015-12-07 16:13:03 -0800

    added procfile

--- omdb/index.js
+++ omdb/index.js
@@ -20,10 +20,35 @@ app.use('/results', resultsPage);
 app.use('/movie', moviePage);
 
 
-// app.use('/', indexPage);
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/movies', function(req, res) {
+  var query = req.query.q;
+
+  debugger
+
+  request('http://www.omdbapi.com/?s=' + query, function(err, response, body) {
+    var data = JSON.parse(body);
+    if (!err && response.statusCode === 200 && data.Search) {
+      res.render('movies', {movies: data.Search,
+                            q: query});
+    } else {
+      res.render('error');
+    }
+  });
+});
+
+app.get('/movies/:imdbID', function(req, res) {
+  // res.send(req.params.imdbID);
+  var searchQuery = req.query.q ? req.query.q : '';
+  var imdbID = req.params.imdbID;
+  request('http://www.omdbapi.com/?i=' + imdbID, function(err, response, body) {
+    res.render('movieShow', {movie: JSON.parse(body),
+                             q: searchQuery});
+  });
+});
 
 
-app.listen(process.env.PORT || 3000)
-
-
-
+app.listen(process.env.PORT || 5000)
