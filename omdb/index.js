27834f1cca350aab9b56e5a5c3a903a3b92f72ae
snilbog <fitch.neal@gmail.com>
2015-12-07 16:02:31 -0800

    no idea

---
+++ omdb/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var	ejsLayouts = require('express-ejs-layouts');
+var request = require('request');
+var app = express();
+
+//controllers
+var indexPage = require('./controller/index.js');
+var resultsPage = require('./controller/results.js');
+var moviePage = require('./controller/movie.js');
+
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/views'));
+
+
+//use controllers
+app.use('/', indexPage);
+app.use('/results', resultsPage);
+app.use('/movie', moviePage);
+
+
+// app.use('/', indexPage);
+
+
+app.listen(process.env.PORT || 3000)
+
+
+
